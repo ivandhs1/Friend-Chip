4f380a54929d642146a1083246850e63b3f80a89
ivandhs <ivanhshs1@gmail.com>
2022-12-08 21:15:55 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/Index/home/home.component';
+import { HomeUserComponent } from './Components/User/home-user/home-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'homeUser', component: HomeUserComponent },
+  { path: '**', pathMatch: 'full', redirectTo: '/home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/Index/home/home.component';
+import { RegisterComponent } from './Components/Index/register/register.component';
+import { LoginComponent } from './Components/Index/login/login.component';
+import { HomeUserComponent } from './Components/User/home-user/home-user.component';
+import { ProfileUserComponent } from './Components/User/profile-user/profile-user.component';
+import { ChatUserComponent } from './Components/User/chat-user/chat-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeUserComponent,
+    ProfileUserComponent,
+    ChatUserComponent
   ],
   imports: [
     BrowserModule,
